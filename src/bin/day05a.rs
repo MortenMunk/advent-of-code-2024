5c3460264f0d41555eba6f2c9d924539d82b0fa1
Morten Munk <mortenmunk97@gmail.com>
2025-10-23 21:30:34 +0200

    kahn

--- src/bin/day05a.rs
+++ src/bin/day05a.rs
@@ -14,7 +14,44 @@ fn init_constraints() -> HashMap<String, HashSet<String>> {
     constraints
 }
 
-fn kahn(line: Vec<&str>) -> i32 {}
+fn kahn(line: &Vec<&str>, constraints: &HashMap<String, HashSet<String>>) -> i32 {
+    let mut l: Vec<String> = Vec::new();
+    let mut s: HashSet<String> = line
+        .iter()
+        .filter(|&&x| constraints.contains_key(x))
+        .map(|&x| x.to_string())
+        .collect();
+
+    let mut indegree: HashMap<String, usize> = HashMap::new();
+    for n in constraints.keys() {
+        indegree.entry(n.clone()).or_insert(0);
+        if let Some(neighbors) = constraints.get(n) {
+            for m in neighbors {
+                *indegree.entry(m.clone()).or_insert(0) += 1;
+            }
+        }
+    }
+
+    while let Some(n) = s.iter().next().cloned() {
+        s.remove(&n);
+        l.push(n.clone());
+
+        if let Some(neighbors) = constraints.get(&n) {
+            for m in neighbors {
+                if let Some(deg) = indegree.get_mut(m)
+                    && *deg > 0
+                {
+                    *deg -= 1;
+                    if *deg == 0 {
+                        s.insert(m.clone());
+                    }
+                }
+            }
+        }
+    }
+
+    l[l.len() / 2].parse::<i32>().unwrap()
+}
 
 fn main() {
     let content: Vec<Vec<&str>> = include_str!("../../inputs/day05b.txt")
@@ -22,5 +59,6 @@ fn main() {
         .map(|x| x.split(',').collect())
         .collect();
     let constraints = init_constraints();
-    println!("{:?}", content)
+    let result: i32 = content.iter().map(|x| kahn(x, &constraints)).sum();
+    println!("{:?}", result)
 }
